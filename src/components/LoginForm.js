4a7eb721321dcb7183bbd7adb080c7dd212b01ee
Daniel-Cross <crossy1686@gmail.com>
2020-12-09 20:23:49 +0100

    added handle method for form submit

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,28 +1,34 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import Button from './Button';
-import Input from './Input';
-import {fields, buttons} from '../config/authForms';
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "./Button";
+import Input from "./Input";
+import { fields, buttons } from "../config/authForms";
 
 const LoginForm = () => {
+  const handleClick = () => {
+    console.log("YOU CLICKED ME");
+  };
+
   return (
-    <div className="form--login custom-form" >
+    <div className="form--login custom-login-form">
       <Button
         text="Log in with Facebook"
         variant="facebook"
-        onClickHandler={() => {}}
+        onClickHandler={handleClick}
       />
       <div className="divider">or</div>
       <Input field={fields.email} />
       <Input field={fields.password} />
-      <Button
-        text={buttons.submitLogIn.text}
-        onClickHandler={() => {}}
-      />
-      <p>Don't have an account? <Link to="/">Sign in</Link>.</p>
-      <p>Forgot password? Click here to <Link to="/reset-password">reset it</Link>.</p>
+      <Button text={buttons.submitLogIn.text} onClickHandler={handleClick} />
+      <p>
+        Don't have an account? <Link to="/">Sign in</Link>.
+      </p>
+      <p>
+        Forgot password? Click here to{" "}
+        <Link to="/reset-password">reset it</Link>.
+      </p>
     </div>
   );
-}
+};
 
 export default LoginForm;
